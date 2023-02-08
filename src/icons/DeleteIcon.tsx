f9d9c1a211d6740f94693499c6dfbef8f270b182
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-02-08 16:39:35 -0300

    Fixed color bug icons

--- src/icons/DeleteIcon.tsx
+++ src/icons/DeleteIcon.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const DeleteIcon = ({ className }: Props) => {
     return (
         <IconContainer className={className}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <line x1="4" y1="7" x2="20" y2="7" />
                 <line x1="10" y1="11" x2="10" y2="17" />
