c8960cf03954d172c06de5dd85c78847437a30a2
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-03-01 16:13:28 -0300

    Added gh pages package and fixed skill view

--- src/modules/portfolio/views/Skill/SkillView.tsx
+++ src/modules/portfolio/views/Skill/SkillView.tsx
@@ -7,12 +7,12 @@ import { SkillContainer } from './SkillContainer';
 export const SkillView = () => {
     const { activeUser } = useSelector((state: RootState) => state.portfolio);
 
-    const { isSameUserParamAuth } = useEdit();
+    const { isLoggedUserProfile, isEditParam } = useEdit();
 
     return (
         <SectionContainer title='Skills'>
             <>
-                {isSameUserParamAuth && <EditButton to='edit/skills' isForSection />}
+                {!isEditParam && isLoggedUserProfile && <EditButton to='edit/skills' isForProfile />}
             </>
             <div className='flex flex-row flex-wrap justify-evenly'>
                 {activeUser.skills.map(skill => <SkillContainer key={skill.id} skill={skill} />)}
